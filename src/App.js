c603a584e1c77323ef422b7e4f48ac6dd31bed7a
Agustin <agustinemmanuel.ml@gmail.com>
2020-07-05 12:06:18 -0300

    Se elimina lib no usada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "pages/HomePage";
 
